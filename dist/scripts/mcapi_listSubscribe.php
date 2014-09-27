da175d78dc6682c2c7b0172db01fe2e2bb415a0d
Fernando Guirao <fernando@bueninvento.es>
2014-09-27 11:50:06 +0200

    últimos cambios

--- dist/scripts/mcapi_listSubscribe.php
+++ dist/scripts/mcapi_listSubscribe.php
@@ -5,7 +5,7 @@ require_once 'config.inc.php';
 
 $api = new MCAPI($apikey);
 
-$merge_vars = array('FNAME'=>$_GET['MERGE1'], 'MMERGE3'=>$_GET['MERGE3'], 'MMERGE4'=>$_GET['MERGE4']);
+$merge_vars = array('FNAME'=>$_GET['MERGE1'], 'MMERGE3'=>$_GET['MERGE3']);
 
 $retval = $api->listSubscribe( $listId, $_GET['MERGE2'], $merge_vars,'html', false );
 
